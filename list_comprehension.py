0c22ecd267b7953f0df11bec356a502230e7aa9b
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-25 11:10:36 -0300

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,2 +1,12 @@
 l1 = [1,2,3,4,5,6,7,8,9]
-l2 = [variavel for variavel in l1]
+ex1 = [variavel for variavel in l1]
+print(ex1)
+print()
+
+ex2 = [v * 2 for v in l1]
+print(ex2)
+print()
+
+ex3 = [(v,v2) for v in l1 for v2 in range(3)]
+print(ex3)
+print()
